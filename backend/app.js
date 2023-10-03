f9655e0c522c9a73dc4623403ad239a70ab9121d
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-10-03 21:25:26 +0200

    some changes

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../backend/services/user");
 const app = express();
 
 // Ustawienie portu, na którym serwer będzie nasłuchiwać
@@ -10,6 +11,16 @@ app.use(express.json());
 // Przykład routingu
 app.get("/", (req, res) => {
   res.send("Witaj na mojej aplikacji Express!");
+});
+
+app.post("/api/register", (req, res) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+    fristName: req.body.fristName,
+  });
+
+  res.status(200).json({ message: "Rejestracja zakończona sukcesem" });
 });
 
 // Start serwera na określonym porcie
