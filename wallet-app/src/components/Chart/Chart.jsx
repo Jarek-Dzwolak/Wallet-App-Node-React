764812d008004efc64a86215c3f1f8f14a625729
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-10-10 15:26:55 +0200

    add Header a fixed

--- wallet-app/src/components/Chart/Chart.jsx
+++ wallet-app/src/components/Chart/Chart.jsx
@@ -1,68 +1,68 @@
-import React, { useEffect, useRef, useState } from "react";
-import Chart from "chart.js/auto";
+// import React, { useEffect, useRef, useState } from "react";
+// import Chart from "chart.js/auto";
 
-const ChartComponent = ({ chartData }) => {
-  const chartRef = useRef(null);
-  const chartInstance = useRef(null);
-  const [canvasDimensions, setCanvasDimensions] = useState({ width: 0, height: 0 });
+// const ChartComponent = ({ chartData }) => {
+//   const chartRef = useRef(null);
+//   const chartInstance = useRef(null);
+//   const [canvasDimensions, setCanvasDimensions] = useState({ width: 0, height: 0 });
 
-  useEffect(() => {
-    if (chartInstance.current) {
-      chartInstance.current.destroy();
-    }
+//   useEffect(() => {
+//     if (chartInstance.current) {
+//       chartInstance.current.destroy();
+//     }
 
-    const myChartRef = chartRef.current.getContext("2d");
+//     const myChartRef = chartRef.current.getContext("2d");
 
-    chartInstance.current = new Chart(myChartRef, {
-      type: "doughnut",
-      data: chartData,
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            enabled: false,
-          },
-          cutout: "70%",
-        },
-      },
-    });
+//     chartInstance.current = new Chart(myChartRef, {
+//       type: "doughnut",
+//       data: chartData,
+//       options: {
+//         responsive: true,
+//         maintainAspectRatio: false,
+//         plugins: {
+//           legend: {
+//             display: false,
+//           },
+//           tooltip: {
+//             enabled: false,
+//           },
+//           cutout: "70%",
+//         },
+//       },
+//     });
 
-    const canvasWidth = chartRef.current.width;
-    const canvasHeight = chartRef.current.height;
+//     const canvasWidth = chartRef.current.width;
+//     const canvasHeight = chartRef.current.height;
 
-    setCanvasDimensions({ width: canvasWidth, height: canvasHeight });
+//     setCanvasDimensions({ width: canvasWidth, height: canvasHeight });
 
-    return () => {
-      if (chartInstance.current) {
-        chartInstance.current.destroy();
-      }
-    };
-  }, [chartData]);
+//     return () => {
+//       if (chartInstance.current) {
+//         chartInstance.current.destroy();
+//       }
+//     };
+//   }, [chartData]);
 
-  return (
-    <div style={{ position: "relative" }}>
-      <canvas ref={chartRef}></canvas>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          textAlign: "center",
-          width: "100%",
-          color: "#000",
-        }}
-      >
-        <div style={{ fontSize: "10px", fontWeight: "bold" }}>
-          Saldo konta: {chartData.accountBalance} PLN
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div style={{ position: "relative" }}>
+//       <canvas ref={chartRef}></canvas>
+//       <div
+//         style={{
+//           position: "absolute",
+//           top: "50%",
+//           left: "50%",
+//           transform: "translate(-50%, -50%)",
+//           textAlign: "center",
+//           width: "100%",
+//           color: "#000",
+//         }}
+//       >
+//         <div style={{ fontSize: "10px", fontWeight: "bold" }}>
+//           Saldo konta: {chartData.accountBalance} PLN
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ChartComponent;
+// export default ChartComponent;
--- wallet-app/src/privateRoute/PrivateRoute.jsx
+++ wallet-app/src/privateRoute/PrivateRoute.jsx
@@ -4,8 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
   const isLoggedIn = localStorage.getItem('accessToken');
 
-  return;
-  isLoggedIn ? <Outlet /> : <Navigate to={'/login'} />;
+  return isLoggedIn ? <Outlet /> : <Navigate to={'/login'} />;
 };
 
 export default PrivateRoute;
