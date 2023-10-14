0d2e63980313ea98293af7288a0cb2d1dc2819f6
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-10-15 00:40:23 +0200

    commit

--- wallet-app/src/components/Balance/Balance.jsx
+++ wallet-app/src/components/Balance/Balance.jsx
@@ -1,29 +1,49 @@
 import styles from './Balance.module.css';
 
-const Balance = () => {
-  const transactions = JSON.parse(localStorage.getItem('transactions'));
-  const calculateTotalSum = (transactions) => {
-    let totalSum = 0;
+import React, { useState, useEffect } from 'react';
 
+const Balance = () => {
+  const [transactions, setTransactions] = useState(
+    JSON.parse(localStorage.getItem('transactions')) || [],
+  );
+  const [totalSum, setTotalSum] = useState(0);
+
+  const calculateTotalSum = (transactions) => {
+    let sum = 0;
     transactions.forEach((transaction) => {
       const sumValue = parseFloat(transaction.Sum);
-
-      if (transaction.Type === '-') {
-        totalSum -= sumValue;
-      } else {
-        totalSum += sumValue;
-      }
+      sum += transaction.Type === '-' ? -sumValue : sumValue;
     });
 
-    return totalSum.toFixed(2);
+    return sum.toFixed(2);
   };
 
-  const totalSum = calculateTotalSum(transactions);
+  useEffect(() => {
+    // Funkcja do obsługi zmiany w localStorage
+    const handleStorageChange = (event) => {
+      console.log(event);
+      if (event.key === 'transactions') {
+        const updatedTransactions = JSON.parse(event.newValue) || [];
+        setTransactions(updatedTransactions);
+      }
+    };
+
+    // Rejestracja nasłuchiwania na zdarzenie zmiany w localStorage
+    window.addEventListener('storage', handleStorageChange);
+
+    // Aktualizacja totalSum po zmianie danych w transactions
+    setTotalSum(calculateTotalSum(transactions));
+
+    // Pamiętaj o usunięciu nasłuchiwania, gdy komponent zostanie odmontowany
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, [transactions]); // Dodanie transactions do zależności useEffect
 
   return (
     <div className={styles.balanceField}>
       <span className={styles.title}>YOUR BALANCE</span>
-      <span className={styles.amount}>₴ {totalSum}</span>
+      <span className={styles.amount}>zł {totalSum}</span>
     </div>
   );
 };
--- wallet-app/src/components/Curriences/Curriences.jsx
+++ wallet-app/src/components/Curriences/Curriences.jsx
@@ -1,10 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Curriences.module.css';
 import bg from '../../icons/curriencesBg.svg';
 import bgTablet from '../../icons/curriencesBgTablet.svg';
 import bgDesktop from '../../icons/curriencesBg.svg';
 
 const Curriences = () => {
-  const isDesktopScreen = window.innerWidth >= 1280;
+  const [usdData, setUsdData] = useState(null);
+  const [eurData, setEurData] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrencyData = async () => {
+      try {
+        // Pobierz dane kursu sprzedaży dla USD
+        const usdResponse = await fetch(
+          'http://api.nbp.pl/api/exchangerates/rates/c/USD/',
+        );
+        const usdData = await usdResponse.json();
+        setUsdData(usdData);
+
+        // Pobierz dane kursu sprzedaży dla EUR
+        const eurResponse = await fetch(
+          'http://api.nbp.pl/api/exchangerates/rates/c/EUR/',
+        );
+        const eurData = await eurResponse.json();
+        setEurData(eurData);
+      } catch (error) {
+        console.error('Error fetching currency data:', error);
+      }
+    };
+
+    fetchCurrencyData();
+  }, []); // Pusta tablica zależności oznacza, że useEffect wykona się tylko raz po zamontowaniu komponentu
+
+  const renderCurrencyRow = (currency, purchaseRate, saleRate) => (
+    <li className={styles.walletValues}>
+      <span className={styles.item}>{currency}</span>
+      <span className={styles.item}>{purchaseRate} zł</span>
+      <span className={styles.item}>{saleRate}zł</span>
+    </li>
+  );
+
   return (
     <ul className={styles.walletBox}>
       <li className={styles.walletKeys}>
@@ -13,18 +48,20 @@ const Curriences = () => {
         <span className={styles.item}>Sale</span>
       </li>
       <ul className={styles.walletValuesBox}>
-        <img className={styles.walletBg} src={bgTablet} alt="bg"></img>
-        <li className={styles.walletValues}>
-          <span className={styles.item}>USD</span>
-          <span className={styles.item}>27.55</span>
-          <span className={styles.item}>27.65</span>
-        </li>
-        <li className={styles.walletValues}>
-          <span className={styles.item}>EUR</span>
-          <span className={styles.item}>30.00</span>
-          <span className={styles.item}>30.10</span>
-        </li>
+        {usdData &&
+          renderCurrencyRow(
+            'USD',
+            usdData.rates[0].ask.toFixed(2),
+            usdData.rates[0].bid.toFixed(2),
+          )}
+        {eurData &&
+          renderCurrencyRow(
+            'EUR',
+            eurData.rates[0].ask.toFixed(2),
+            eurData.rates[0].bid.toFixed(2),
+          )}
       </ul>
+      <img className={styles.walletBg} src={bgTablet} alt="bg"></img>
     </ul>
   );
 };
